c0cfedd32ef4a92a789d6a23cef2b76459fd9d59
GaoJie <89476940+GaoJie0648@users.noreply.github.com>
2023-12-06 14:49:06 +0800

    新增 對照 與 htaccess

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // 監聽圖片進入畫面時載入高畫質圖片
 // 
 // ==== IntersectionObserver ====
-function onEnterView(entries, observer) {
-    for (const entry of entries) {
-        if (entry.isIntersecting) {
+window.onload = () => {
+    function onEnterView(entries, observer) {
+        for (const entry of entries) {
             const img = entry.target;
-            img.classList.add('loading')
-            img.setAttribute('src', img.dataset.src);
-            observer.unobserve(img);
+            if (entry.isIntersecting) {
+                img.classList.add('loading')
+                img.setAttribute('src', img.dataset.src);
+                // observer.unobserve(img);
+            }else{
+                img.classList.remove('loading')
+                img.setAttribute('src',img.dataset.small)
+            }
         }
     }
-}
-const watcher = new IntersectionObserver(onEnterView);
-const lazyImages = document.querySelectorAll('img.lazy');
-for (const image of lazyImages) {
-    watcher.observe(image);
+    const watcher = new IntersectionObserver(onEnterView);
+    const lazyImages = document.querySelectorAll('img.lazy');
+    for (const image of lazyImages) {
+        watcher.observe(image);
+    }
 }
 // ========
